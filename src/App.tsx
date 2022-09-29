457eaa39105efbba6799ae72db00d285dc92cd4b
Duc Thai <ducthai060501@gmail.com>
2022-09-29 23:28:03 +0300

    set up

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+
+function App() {
+  
+
+  return (
+    <div  className='text-3xl font-bold underline'>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      spacing: {
+        '2/3': '66.666667%',
+      },
+    },
+  },
+  plugins: [],
+}
