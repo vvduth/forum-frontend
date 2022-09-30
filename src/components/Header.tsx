46d6c0d9bd01031f10983427224df4cc70414a0d
Duc Thai <ducthai060501@gmail.com>
2022-09-30 03:41:53 +0300

    monor change in styling and background

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,10 +10,10 @@ const Header = () => {
       <div className="border-b w-full inline-block border-blue-400 py-8">
         <div className="md:float-left block">
           <a href="/">
-            <span className="cursor-pointer font-bold text-4xl">PukeDuke</span>
+            <span className="cursor-pointer text-white font-bold text-4xl">PukeDuke</span>
           </a>
         </div>
-        <div className="hidden md:float-left md:contents">
+        <div className="hidden md:float-left md:contents text-white">
           {categories.map((category: any, index: number) => (
             <a key={index}>
               <span className="md:float-right mt-2 align-middle ml-4 font-semibold cursor-pointer">
@@ -29,26 +29,27 @@ const Header = () => {
             onClick={() => setToggle((prev) => !prev)}
           >
             {toggle ? (
-              <AiOutlineMenu className="w-[28px] h-[28px]" />
-            ) : (
               <AiOutlineCloseCircle className="w-[28px] h-[28px]" />
+            ) : (
+              
+              <AiOutlineMenu className="w-[28px] h-[28px]" />
             )}
           </button>
           <br />
-          <div className={`${!toggle ? "flex" : "hidden"}`}>
+          <div className={`${toggle ? "flex" : "hidden"}`}>
             <ul className="flex flex-col justify-end flex-1 items-end">
               {categories.map((category: string, i: number) => (
                 <li
                   key={i}
-                  className="border-b shadow-md border-blue-500 p-2 rounded-full"
+                  className="border-b shadow-md border-pink-500 bg-pink-500 m-2 p-2 rounded-full text-white font-bold"
                 >
                   {category}
                 </li>
               ))}
-              <li className="border-b shadow-md bg-lime-800 text-2xl font-bold text-white mt-2 border-blue-500 p-2 rounded-full">
+              <li className="border-b shadow-md bg-yellow-400 text-2xl font-bold text-white mt-2 border-blue-500 p-2 rounded-full">
                 Login
               </li>
-              <li className="border-b shadow-md bg-lime-800 text-2xl font-bold text-white mt-2 border-blue-500 p-2 rounded-full">
+              <li className="border-b shadow-md bg-yellow-400 text-2xl font-bold text-white mt-2 border-blue-500 p-2 rounded-full">
                 Sign up{" "}
               </li>
             </ul>
