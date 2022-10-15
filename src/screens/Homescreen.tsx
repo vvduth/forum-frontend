ba090c08e956d5720f8a8c5c74a141c38eff0006
Duc Thai <ducthai060501@gmail.com>
2022-10-15 22:14:00 +0300

    play around #2

--- src/screens/Homescreen.tsx
+++ src/screens/Homescreen.tsx
@@ -1,33 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FeaturePost from "../components/FeaturePost";
 import { featuredPost } from "../data/Dummy_data";
 import { IPost } from "../types/types";
 import PostCard from "../components/PostCard";
-
+import { getPosts } from "../service";
 import { Route } from "react-router-dom";
 
 import Layout from "../components/Layout";
 const Homescreen = () => {
+  const [allPost, setAllPost] = useState<any>(null) ; 
+  const fetchAllPost = async () => {
+    const res = await getPosts() as any ; 
+    setAllPost(res[0]) ;
+    console.log(res[0])
+  }
+
+  useEffect(()=> {
+    fetchAllPost() ; 
+    console.log(allPost)
+  },[])
   return (
-    // <div className="container mx-auto px-10 mb-8">
-    //   <FeaturePost />
-    //   <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-    //     <div className="lg:col-span-8 col-span-1">
-
-    //       {featuredPost.map((post:IPost)=> (
-    //         <PostCard key={post.id} post= {post} /> 
-    //         ))}
-
-    //     </div>
-    //     <div className="lg:col-span-4 col-span-1">
-    //       <div className="lg:sticky relative top-8">
-    //         <PostWidget /> 
-    //         <Categories /> 
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
+    
     <Layout>
+      <> { allPost ? (<><p>Yes</p></>): (<><p>no</p></>)}</>
       {featuredPost.map((post:IPost)=> (
           <PostCard key={post.id} post= {post} /> 
             ))}
--- src/service/index.ts
+++ src/service/index.ts
@@ -1,33 +1,35 @@
-import {request, gql }  from "graphql-request";
+import { request, gql } from "graphql-request";
+
+const graphqlAPI = `https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/cl99wxkjm2rpc01ue0ugb8gsu/master`;
 
 export const getPosts = async () => {
-    const query = gql`
-      query MyQuery {
-        postsConnection {
-          edges {
-            cursor
-            node {
-              author {
-                bio
-                name
-                id
-                photo {
-                  url
-                }
-              }
-              createdAt
-              slug
-              title
-              excerpt
-              featuredImage {
+  const query = gql`
+    query MyQuery {
+      postsConnection {
+        edges {
+          node {
+            author {
+              bio
+              name
+              id
+              photo {
                 url
               }
-              categories {
+            }
+            createdAt
+            slug
+            excerpt
+            name
+            categories {
                 name
                 slug
-              }
             }
           }
         }
       }
-    `;
+    }
+  `;
+  const result = await request(graphqlAPI, query);
+
+  return result.postsConnection.edges;
+};
